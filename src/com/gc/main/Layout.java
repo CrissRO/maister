ba128460290e9ad43872ca92ebc6696285673b3c
CrissRO <cezarmocanu2014@gmail.com>
2019-11-04 22:44:17 +0100

    [iss2] Added Layout and Player class

---
+++ src/com/gc/main/Layout.java
@@ -0,0 +1,10 @@
+package com.gc.main;
+
+public class Layout {
+
+	private Tile matrix[][];
+	
+	public Layout() {
+		matrix = new Tile[7][7];
+	}
+}
---
+++ src/com/gc/main/Player.java
@@ -0,0 +1,22 @@
+package com.gc.main;
+
+import java.util.List;
+
+public class Player {
+
+	private String name;
+	private String color;
+	private int nrWands;
+	private Recipe secretRecipe;
+	private List<Ingredient> collectedIngredients;
+	private Tile location;
+	
+	
+	public Player(String name,String color) {
+		this.name = name;
+		this.color = color;
+	}
+	
+	public void move() {}
+	public void useWand() {}
+}
